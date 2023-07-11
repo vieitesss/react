a1358c5d0b3974592059d451490a310fce92bc8d
Daniel Vieites <dvieitest@gmail.com>
2023-07-11 12:06:54 +0200

    ya funciona el currentBoard en el componente Sudoku

--- sudoku/src/components/Celda.js
+++ sudoku/src/components/Celda.js
@@ -1,30 +1,6 @@
 import { PropTypes } from 'prop-types';
-import { useEffect, useState } from "react";
 
 export const Celda = ({ id, borderStyle, isSelected, onSelect, currentNumber, isBlocked }) => {
-
-    const [number, setNumber] = useState(currentNumber);
-
-    useEffect(() => {
-        setNumber(currentNumber)
-    }, [currentNumber])
-
-    useEffect(() => {
-        window.addEventListener("keydown", handleKeyUp);
-
-        return () => {
-            window.removeEventListener("keydown", handleKeyUp);
-        }
-    }, [isSelected])
-
-    const handleKeyUp = e => {
-        const n = Number(e.key)
-
-        if (!Number.isNaN(n) && isSelected && !isBlocked) {
-            setNumber(n);
-        }
-    }
-
     return (
         <div id={id}
             className={"celda"
@@ -32,7 +8,7 @@ export const Celda = ({ id, borderStyle, isSelected, onSelect, currentNumber, is
                 + (isSelected ? " selected" : "")
                 + (isBlocked ? " blocked" : "")}
             onClick={e => onSelect(e, id)}>
-            {number != 0 ? number : ""}
+            {currentNumber != 0 ? currentNumber : ""}
         </div>
     )
 }
@@ -43,5 +19,5 @@ Celda.propTypes = {
     isSelected: PropTypes.bool,
     onSelect: PropTypes.func,
     currentNumber: PropTypes.number,
-    isBlocked: PropTypes.bool
+    isBlocked: PropTypes.bool,
 }
--- sudoku/src/components/Sudoku.js
+++ sudoku/src/components/Sudoku.js
@@ -4,31 +4,89 @@ import { Celda } from "./Celda"
 
 export const Sudoku = ({ flag, handleError }) => {
     const [puzzle, setPuzzle] = useState([]);
+    const [currentBoard, setCurrentBoard] = useState([]);
     const [solution, setSolution] = useState([]);
     const [difficulty, setDifficulty] = useState("");
     const [selectedCell, setSelectedCell] = useState([-1, -1]);
     const [loading, setLoading] = useState(false);
-
-    const getPuzzle = async () => {
-        return fetch("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution,difficulty},results,message}}")
-            .then(data => data.json());
+    
+    const staticPuzzle = () => {
+        return [
+            [0,9,0,0,0,4,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,5,8,9,1,3,0,0],
+            [0,0,0,0,0,0,0,0,7],
+            [0,0,0,0,0,0,5,4,0],
+            [0,0,0,0,7,0,0,0,0],
+            [0,2,0,0,0,5,0,7,3],
+            [0,0,6,3,0,0,0,9,1],
+            [0,3,0,0,0,0,0,0,0],
+        ]
     }
 
     useEffect(() => {
-        const controller = new AbortController()
-        setLoading(true)
-        getPuzzle()
-            .then(items => {
-                const sudoku = items.newboard.grids[0];
-                setPuzzle(sudoku.value)
-            })
-            .catch(err => handleError(err))
-            .finally(() => setLoading(false))
+        console.log("reset puzzle");
+        const newPuzzle = staticPuzzle();
+        setPuzzle(newPuzzle.slice());
+        setCurrentBoard(newPuzzle.slice());
+    }, [flag])
+
+    useEffect(() => {
+        console.log(puzzle === currentBoard)
+    }, [puzzle])
+
+    // const getPuzzle = async () => {
+    //     return fetch("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution,difficulty},results,message}}")
+    //         .then(data => data.json());
+    // }
+
+    // useEffect(() => {
+    //     const controller = new AbortController()
+    //     setLoading(true)
+    //     getPuzzle()
+    //         .then(items => {
+    //             const sudoku = items.newboard.grids[0];
+    //             setPuzzle(sudoku.value)
+    //         })
+    //         .catch(err => handleError(err))
+    //         .finally(() => setLoading(false))
+
+    //     return () => {
+    //         controller.abort()
+    //     }
+    // }, [flag])
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
 
         return () => {
-            controller.abort()
+            window.removeEventListener("keydown", handleKeyDown);
         }
-    }, [flag])
+    }, [selectedCell])
+
+    const handleKeyDown = e => {
+        const n = Number(e.key)
+
+        if (!Number.isNaN(n) && isNotBlocked()) {
+            setCurrentBoard(prev => {
+                const newBoard =  prev.map((array, row) => {
+                    return array.map((value, col) => {
+                        if (row == selectedCell[0] && col == selectedCell[1]) {
+                            return n;
+                        } 
+
+                        return value;
+                    });
+                })
+
+                return newBoard;
+            })
+        }
+    }
+
+    const isNotBlocked = () => {
+        return puzzle[selectedCell[0]][selectedCell[1]] == 0;
+    }
 
     const onSelect = (_, cell) => {
         setSelectedCell([cell[0], cell[1]]);
@@ -50,7 +108,7 @@ export const Sudoku = ({ flag, handleError }) => {
     return (
         <>
             {
-                loading && <p>"Getting puzzle..."</p>
+                loading && <p>Getting puzzle...</p>
             }
             <div className="sudoku">
                 {
@@ -61,7 +119,7 @@ export const Sudoku = ({ flag, handleError }) => {
                                 borderStyle={borderStyle(row, col)}
                                 isSelected={selectedCell[0] == row && selectedCell[1] == col}
                                 onSelect={onSelect}
-                                currentNumber={(puzzle === undefined || puzzle == 0) ? 0 : puzzle[row][col]}
+                                currentNumber={(currentBoard === undefined || currentBoard == 0) ? 0 : currentBoard[row][col]}
                                 isBlocked={(puzzle === undefined || puzzle == 0) ? false : 
                                     (puzzle[row][col] == 0 ? false : true)}
                             />
